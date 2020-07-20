5c9137d108c0a50c9d3022fa0e7f0e1f6fb08db3
Nishlanka <nishlankaperera@gmail.com>
2020-07-21 01:19:16 +0530

    Layouts set to laravel

---
+++ Jobs_project/resources/views/layouts/footer.blade.php
@@ -0,0 +1,13 @@
+<!-- script files -->
+<script src="./jquery.js.download"></script>
+<script src="./what-input.js.download"></script>
+<script src="./foundation.js.download"></script>
+<script src="./jquery.showmore.src.js.download" type="text/javascript"></script>
+<script src="./app.js.download"></script>
+<script src="./greensock.js.download" type="text/javascript"></script>
+<!-- LayerSlider script files -->
+<script src="./layerslider.transitions.js.download" type="text/javascript"></script>
+<script src="./layerslider.kreaturamedia.jquery.js.download" type="text/javascript"></script>
+<script src="./owl.carousel.min.js.download"></script>
+<script src="./inewsticker.js.download" type="text/javascript"></script>
+<script src="./jquery.kyco.easyshare.js.download" type="text/javascript"></script>
